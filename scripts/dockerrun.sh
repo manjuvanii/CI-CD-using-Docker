1ff20a72d64d5ab6f1d0c6660570aa0ce2bcc8d5
peyyala7hills <101161227+peyyala7hills@users.noreply.github.com>
2023-06-07 19:22:39 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,2 +1,2 @@
-sudo docker pull yedukondalu212/samplejavaapp:latest
-sudo docker run -itd -p 8003:8080 yedukondalu212/samplejavaapp:latest
+sudo docker pull yedukondalu212/java-app-demo:latest
+sudo docker run -itd -p 8003:8080 yedukondalu212/java-app-demo:latest
