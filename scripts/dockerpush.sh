96cc76e6928e54dfd2a3f50fb718f9ee8f54a08e
peyyala7hills <101161227+peyyala7hills@users.noreply.github.com>
2023-06-07 19:36:10 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,3 +1,4 @@
 
-sudo docker login --username=$username --password=$password
+#sudo docker login --username=$username --password=$password
+sudo docker login -u$username -p$password
 sudo docker push yedukondalu212/java-app-demo:latest
