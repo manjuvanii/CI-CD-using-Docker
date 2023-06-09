38163e455fb5318db0a972578f120cc091ae5f8d
manjuvanii <55694899+manjuvanii@users.noreply.github.com>
2023-06-09 15:14:40 +0530

    Update dockerpush.sh

--- scripts/dockerpush.sh
+++ scripts/dockerpush.sh
@@ -1,5 +1,5 @@
 
 #sudo docker login --username=$username --password=$password
 sudo docker logout
-cat ~/password.txt | sudo docker login --username yedukondalu212 --password-stdin
-sudo docker push yedukondalu212/java-app-demo:latest
+cat ~/password.txt | sudo docker login --username manjuvani5699 --password-stdin
+sudo docker push manjuvani5699/java-app-demo:latest
