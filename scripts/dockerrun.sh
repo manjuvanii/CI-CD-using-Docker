696f2ab68f58b01bff2300fe8f839bc3028bf735
manjuvanii <55694899+manjuvanii@users.noreply.github.com>
2023-06-09 15:16:06 +0530

    Update dockerrun.sh

--- scripts/dockerrun.sh
+++ scripts/dockerrun.sh
@@ -1,3 +1,3 @@
-sudo docker pull yedukondalu212/java-app-demo:latest
+sudo docker pull manjuvani5699/java-app-demo:latest
 sudo docker rm -f javaapp
-sudo docker run -itd -p 8004:8080 --name javaapp yedukondalu212/java-app-demo:latest
+sudo docker run -itd -p 8004:8080 --name javaapp manjuvani5699/java-app-demo:latest
